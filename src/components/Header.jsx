cc8d0c2d799ba14f0e562662103afb4af7cf44e2
Mahfuz Twoha <mahfuzztwohaa@gmail.com>
2024-02-10 16:51:38 +0600

    modified

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -30,7 +30,7 @@ export default function Header() {
       <header id="main-header">
         <div id="main-title">
           <img src="img/logo.png" alt="Elegant model" />
-          <h1>Elegant Context</h1>
+          <h1>ToxiCoder Elegant Context</h1>
         </div>
         <p>
           <button onClick={handleOpenCartClick}>Cart ({cartQuantity})</button>
